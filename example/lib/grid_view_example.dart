d6e55af46115da8f793c0bf982b855ecb61f2516
YudizAndroidVarshil <varshil.s@yudiz.com>
2024-09-06 13:45:39 +0530

    added support for dynamic height grid view

---
+++ example/lib/grid_view_example.dart
@@ -0,0 +1,34 @@
+import 'package:dynamic_height_list_view/dynamic_height_grid_view.dart';
+import 'package:flutter/material.dart';
+
+class GridViewExample extends StatelessWidget {
+  GridViewExample({super.key});
+
+  final List<Color> colors = List.generate(100, (index) => Color((index * 0xFFFFFF ~/ 100) << 0).withOpacity(1.0));
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dynamic Height Grid View'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: DynamicHeightGridView(
+          shrinkWrap: true,
+          itemCount: 100,
+          crossAxisCount: 3,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          builder: (context, index) {
+            return Container(
+              height: 200,
+              color: colors[index],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ example/lib/list_view_example.dart
@@ -0,0 +1,29 @@
+import 'package:dynamic_height_list_view/dynamic_height_list_view.dart';
+import 'package:flutter/material.dart';
+
+class ListViewExample extends StatelessWidget {
+  ListViewExample({super.key});
+
+  final List<Color> colors = List.generate(100, (index) => Color((index * 0xFFFFFF ~/ 100) << 0).withOpacity(1.0));
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dynamic Height List View'),
+      ),
+      body: DynamicHeightListView<int>(
+        items: List.generate(10, (index) => index),
+        itemPadding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+        itemBuilder: (context, item) => Card(
+          child: Container(
+            height: 40,
+            width: 100,
+            color: colors[item],
+          ),
+        ),
+      ),
+    );
+  }
+}
